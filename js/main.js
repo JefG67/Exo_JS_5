36a3c0deba55e48f8c32b40a075f0d7f9e9a88a5
JefG67 <jefguilpain@gmail.com>
2024-02-27 14:20:52 +0100

    terminé et commenté

--- js/main.js
+++ js/main.js
@@ -9,9 +9,16 @@ boxCitation.classList.add("boxCitation")
 quotes.forEach(function(quote) { // Je déclare une boucle pour la création des citations
     const newBox = boxCitation.cloneNode(); // Clone des divs
     let text = "<p>" + quote.content + "</p>" +
-                "<p>" + quote.author + "</p>";
+                "<p class= 'auteur' >" + quote.author + "</p>" +
+                "<i id='coeur' class='fa-solid fa-heart'></i>" // ajout du coeur 
         
 
         newBox.innerHTML = text // J'ajoute l'html a la box
         board.appendChild(newBox);
+        let coeur = newBox.querySelector(".fa-heart") //je selection les coeurs de mes box
+        coeur.addEventListener("click",function(){ //j'ajoute un evenement click sur chaque coeur
+                coeur.classList.toggle("clicked")
+        
+        }
+        )
 })
--- js/quotes.js
+++ js/quotes.js
@@ -1,4 +1,35 @@
-let quotes = [{
-    author:"georges",
-    content: "le train de tes injures qsdqsd",
-}]
+let quotes = 
+    [{
+    author:"Serge Karamazov",
+    content: "Barrez vous **** de mimes.",
+    },
+
+    {
+    author: "Georges Abitbol",
+    content: "Le train de tes injures roule sur le rail de mon indifférence.",
+    },
+    {
+    author:"Hubert Bonisseur de la bath",
+    content:"Ça ne prenait pas beaucoup plus de temps.",    
+    },
+    {
+    author:"Moulinier",    
+    content:"l'habit ne fait pas le moine, mais il fait l'agent...Même non titularisé." ,   
+    },
+    {
+    author:"Orson Welles",
+    content:"C'est du vol et du plagiat.J'aime pas trop les voleurs et...",     
+
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
